4f084374e82ca6ec9c9be43e9dc7c5fc4af5345f
Pranta dhar <159190041+Pranta19@users.noreply.github.com>
2025-11-19 12:23:19 +0600

    load data

--- code.py
+++ code.py
@@ -2,3 +2,6 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
+# 1️⃣ Load data
+df_train = pd.read_csv(r"D:\ml 15 days\kaggle compitition2\train.csv")
+df_test  = pd.read_csv(r"D:\ml 15 days\kaggle compitition2\test.csv")
