ead26c4141af054264c2f3b14c08e31bac1b06ca
Pranta dhar <159190041+Pranta19@users.noreply.github.com>
2025-11-19 12:24:46 +0600

    separate id

--- code.py
+++ code.py
@@ -5,3 +5,12 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 # 1️⃣ Load data
 df_train = pd.read_csv(r"D:\ml 15 days\kaggle compitition2\train.csv")
 df_test  = pd.read_csv(r"D:\ml 15 days\kaggle compitition2\test.csv")
+# 2️⃣ Separate target and test ids
+X_train = df_train.drop(columns=['loan_paid_back'])
+y_train = df_train['loan_paid_back']
+
+if 'id' in df_test.columns:
+    test_ids = df_test['id']
+    df_test = df_test.drop(columns=['id'])
+else:
+    test_ids = df_test.index
